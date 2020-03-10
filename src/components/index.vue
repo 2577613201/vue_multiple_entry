77f9aace08cc85e5092853c1d8434fb5514ae94f
zbm <2577613201@qq.com>
2020-03-10 15:25:52 +0800

    vue多入口多出口

---
+++ src/components/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+   	<p>这是index页面</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
---
+++ src/components/other.vue
@@ -0,0 +1,9 @@
+<template>
+	<p>这是多入口的一个页面</p>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
